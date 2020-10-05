e233ee642176e2be7a0a8dd95bb768ae174ed7e5
itizidon <dng21@binghamton.edu>
2020-10-05 17:11:51 -0400

    created basic go server

---
+++ wiki.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
